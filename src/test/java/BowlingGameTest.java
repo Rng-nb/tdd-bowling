6577b81ab0842c7294c3f99a80aa3ae440b86dd5
Rng-nb <18782824515@163.com>
2020-08-04 00:05:50 +0800

    tdd

--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,45 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class BowlingGameTest {
+    @Test
+    void should_return_total_number_when_getscore_given_without_strike_nor_spare_anyFrame() {
+        String hitPin[] = {"1/1","1/1","1/1","1/1","1/1","1/1","1/1","1/1","1/1","1/1"};
+        BowlingGame bowlingGame = new BowlingGame();
+        int score = bowlingGame.getScore(hitPin);
+        assertEquals(20, score);
+    }
+
+    @Test
+    void should_return_sum_number_when_getscore_given_with_strike_but_not_continuous_except_10thFrame() {
+        String hitPin[] = {"1/1","1/1","Strike/null","1/1","1/1","Strike/null","1/1","1/1","1/1","1/1"};
+        BowlingGame bowlingGame = new BowlingGame();
+        int score = bowlingGame.getScore(hitPin);
+        assertEquals(40, score);
+    }
+
+    @Test
+    void should_return_sum_number_when_getscore_given_with_strike_cotinuous_except_10thFrame() {
+        String hitPin[] = {"1/1","1/1","Strike/null","Strike/null","Strike/null","1/1","1/1","1/1","1/1","1/1"};
+        BowlingGame bowlingGame = new BowlingGame();
+        int score = bowlingGame.getScore(hitPin);
+        assertEquals(77, score);
+    }
+
+    @Test
+    void should_return_sum_number_when_getscore_given_with_stike_and_spare_except_10thFrame() {
+        String hitPin[] = {"1/1","1/1","Strike/null","Strike/null","Strike/null","1/Spare","Strike/null","1/1","1/1","1/1"};
+        BowlingGame bowlingGame = new BowlingGame();
+        int score = bowlingGame.getScore(hitPin);
+        assertEquals(113, score);
+    }
+
+    @Test
+    void shoul_return_sum_number_when_getscore_given_with_strike_and_spare_with_10th_Strike() {
+        String hitPin[] = {"1/1","1/1","Strike/null","Strike/null","Strike/null","1/Spare","Strike/null","1/1","1/1","Strike/null","Strike/null","Strike/null"};
+        BowlingGame bowlingGame = new BowlingGame();
+        int score = bowlingGame.getScore(hitPin);
+        assertEquals(113, score);
+    }
+
 }
